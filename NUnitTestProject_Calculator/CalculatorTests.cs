4b59818ec2788480731a55609c3dc11ce6b35c6b
Yiseul Ko <gls20020128@gmail.com>
2022-03-25 12:41:31 -0400

    Refactor a and b for test class

--- NUnitTestProject_Calculator/CalculatorTests.cs
+++ NUnitTestProject_Calculator/CalculatorTests.cs
@@ -6,49 +6,48 @@ namespace NUnitTestProject_Calculator
     public class CalculatorTests
     {
         private Calculator calculator;
+        int a, b, expected;
 
         [SetUp]
         public void Setup()
         {
             calculator = new Calculator();
+            a = 20;
+            b = 5;
         }
 
         [Test]
         public void Addition_Test()
         {
-            int a = 10, b = 13;
-            int expected = 9;
+            expected = 9;
             Assert.AreEqual(expected, calculator.Add(a, b));
         }
 
         [Test]
         public void Subtraction_Test()
         {
-            int a = 20, b = 13;
-            int expected = 9;
+            expected = 9;
             Assert.AreEqual(expected, calculator.Subtract(a, b));
         }
 
         [Test]
         public void Multiplication_Test()
         {
-            int a = 10, b = 2;
-            int expected = 9;
+            expected = 9;
             Assert.AreEqual(expected, calculator.Muliply(a, b));
         }
 
         [Test]
         public void Division_Test()
         {
-            int a = 20, b = 2;
-            int expected = 9;
+            expected = 9;
             Assert.AreEqual(expected, calculator.Divide(a, b));
         }
 
         [Test]
         public void ZeoDivisionException_Test()
         {
-            int a = 10, b = 0;
+            b = 0;
             Assert.Throws<DivideByZeroException>(() => calculator.Divide(a, b));
         }
     }
